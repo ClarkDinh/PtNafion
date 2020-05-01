08ba2bad2b45abad007a6e258f78a0c429f8da2e
Nguyen Duong Nguyen <nguyennguyenduong@dp063-028.jaist.ac.jp>
2020-05-01 13:59:08 +0900

    initial commit

---
+++ dmin.py
@@ -0,0 +1,240 @@
+import os
+import shutil
+import pandas as pd
+import numpy as np
+import math
+
+from constant import *
+from rolls import roll_full_k
+from plot_density import plot_density, makedirs
+from preprocess import get_img_idx_given_z, get_xyz
+
+import multiprocessing
+from functools import partial
+
+
+def get_surf_lbl(main_cat, sub_cat):
+	"""
+	NOTICE
+	This function is temporary moved to preprocess.py/get_surf_lbl
+	The function in preprocess.py called by main.py/plot_mix_hists_scdata to
+	create surface label dataset like "csv_surf_dist_morphology_fresh, 21k, 34k"
+	Please do not call this func unless surface labels are estimated by Pt/Co density or Pt valence
+
+	"""
+	# store all z_raw files
+	csv_dir = "{0}/csv/{1}/{2}".format(input_dir, main_cat, sub_cat)
+	list_zfiles = os.listdir(csv_dir)
+	list_zfiles.sort() 
+
+	f1 = lambda x, y, min_value: x - y if x - y > min_value else min_value
+	f2 = lambda x, y, max_value: x + y if x + y < max_value else max_value
+
+ 
+	env_list = []
+	# loc_idx_all = []
+	# scan for all points, but layer by layer
+	for z in range(z_size): 
+		n_neighbors = 1
+		z_minus = f1(z, n_neighbors, 0) # lower bound for z
+		z_plus = f2(z, n_neighbors, z_size) # upper bound for z
+
+		# get nearest neighbor by z dir
+		z_env_all_df = [list_zfiles[t] for t in range(z_minus, z_plus)]
+		if z_plus < z_size:
+			z_env_all_df.append(list_zfiles[z_plus])
+
+		
+		this_env = None
+		for z_file in z_env_all_df:
+			z_df = pd.read_csv(str(input_dir + "/csv/" + main_cat + "/" + sub_cat + "/" + z_file), index_col=0)
+			z_mt = z_df.values
+			# z_rolls = roll(z_mt, k_step=n_neighbors) # shift for all x and y
+			z_rolls = roll_full_k(z_mt, k_steps=n_neighbors) # shift for all x and y
+
+			if this_env is None:
+				this_env = z_rolls
+			else:
+				this_env = np.concatenate((this_env, z_rolls), axis=-1)
+			# print ("z:", z, "z_rolls.shape:", z_rolls.shape, "len(z_rolls):", len(z_rolls))
+		# print (this_env, this_env.shape)
+		env_list.append(this_env) # test lai doan them nay, done, roll_full_k solved
+		
+		# means = np.nanmean(this_env, axis=-1)
+
+		check_nan = np.isnan(np.sum(this_env, axis=-1))
+		surf_lbl = list(map(lambda x: 1 if x else 0, check_nan))
+		
+		
+		# z_df_out = pd.DataFrame(np.array(surf_lbl).reshape(y_size, x_size), index=z_df.index, columns=z_df.columns) 
+		# plot_density(z_df_out.values, save_at="{0}/tmp.pdf".format(cwd),  cmap_name='jet', vmin=0, vmax=0.0003)
+		
+
+		this_zidx, this_zloc_idx = get_img_idx_given_z(zval=z) # # remove it
+		z_df_out = pd.DataFrame(surf_lbl, index=this_zloc_idx, columns=["is_surface"]) #
+
+		positions = np.array(list(map(lambda x: get_xyz(x), this_zloc_idx)))
+		z_df_out["x"] = positions[:,0]
+		z_df_out["y"] = positions[:,1]
+		z_df_out["z"] = positions[:,2]
+
+		save_at = "{0}/csv_surf_dist_{1}_test/layer_{2}.csv".format(result_dir, main_cat, z)
+		makedirs(save_at)
+		z_df_out.to_csv(save_at)
+		print (z)
+		# break
+
+def poolcontext(*args, **kwargs):
+	pool = multiprocessing.Pool(*args, **kwargs)
+	yield pool
+	pool.terminate()
+
+
+def get_index_submatrix_3d(r, c, z, n_neighbor,
+						maxrow, maxcol, max_z):
+
+
+	f1 = lambda x, y, min_value: x - y if x - y > min_value else min_value
+	north = f1(r, n_neighbor, 0)
+	west = f1(c, n_neighbor, 0)
+	up = f1(z, n_neighbor, 0)
+
+	f2 = lambda x, y, max_value: x + y if x + y < max_value else max_value
+	south = f2(r, n_neighbor, maxrow)
+	east = f2(c, n_neighbor, maxcol)
+	down = f2(z, n_neighbor, max_z)
+
+	return east, west, north, south, up, down
+
+
+
+
+
+
+def chec_surf_in_enf(env_df, center_vec):
+	center_vec = np.array(center_vec)
+	if len(env_df) != 0: # if env_df is non-empty set
+		env_idxes = env_df.index
+		# find min distance and nearest surf point
+		for this_i, env_idx in enumerate(env_idxes):
+			this_x = env_df.loc[env_idx, "x"]
+			this_y = env_df.loc[env_idx, "y"]
+			this_z = env_df.loc[env_idx, "z"]
+			this_vec = np.array([this_x, this_y, this_z])
+			this_d = np.linalg.norm(this_vec - center_vec)
+			if this_i == 0:
+				dmin = this_d
+
+			if this_d < dmin:
+				dmin = this_d
+				nearest_point = this_vec
+	else:
+		dmin = np.nan
+	return dmin
+
+
+
+def get_d2surf(x, y, z, list_zfiles, n_neighbor):
+
+	east, west, north, south, z_up, z_down = get_index_submatrix_3d(r=int(y), c=int(x), z=int(z), 
+				n_neighbor=n_neighbor,
+				maxrow=y_size, maxcol=x_size,
+				max_z=z_size)
+
+	z_env_all_df = [list_zfiles[t] for t in range(z_up, z_down)]
+	if z_down < z_size:
+		z_env_all_df.append(list_zfiles[z_down])
+
+	# loop for all z layers
+	dmins = []
+	for z_file in z_env_all_df:
+		z_df = pd.read_csv("{0}/csv_surf_dist/{1}".format(result_dir, z_file), index_col=0)
+
+		surf_df = z_df[z_df["is_surface"] == 1]
+
+		env_df = surf_df[(((surf_df["x"] >= west) & (surf_df["x"] <= east))
+							 & ((surf_df["y"] >= north) & (surf_df["y"] <= south))
+							 & ((surf_df["z"] >= z_up) & (surf_df["z"] <= z_down)))]
+
+		dmin = chec_surf_in_enf(env_df=env_df, center_vec=[x, y, z])
+		dmins.append(dmin)
+
+	return dmins
+
+
+def get_dmin(bidx, bulk_df, list_zfiles):
+	x, y, z = bulk_df.loc[bidx, ["x", "y", "z"]]
+	found = False
+	n_neighbor = 1
+	while not found:
+		dmins = get_d2surf(x=x, y=y, z=z, list_zfiles=list_zfiles, n_neighbor=n_neighbor)
+		dmin_val = np.nanmin(dmins)
+		print (x, y, z, "n_neighbor:",n_neighbor, "dmins:", dmins, "dmin_val:", dmin_val)
+		if math.isnan(dmin_val):
+			n_neighbor += 1
+		else:
+			# df.loc[bidx, "dist2surf"] = dmin_val
+			found = True
+	return dmin_val
+
+def get_dmins():
+	csv_dir = "{0}/csv_surf_dist".format(result_dir)
+	list_zfiles = os.listdir(csv_dir)
+	list_zfiles.sort() 
+
+
+	for z in range(z_size): 
+		file_name = "{0}/csv_surf_dist/layer_{1}.csv".format(result_dir, z)
+		df = pd.read_csv(file_name, index_col=0)
+
+		df["dist2surf"] = 0
+
+		bulk_df = df[df["is_surface"] == 0]
+		bulk_idxs = list(bulk_df.index)
+
+		print (bulk_idxs[:10])
+
+		for bidx in bulk_idxs:
+			x, y, z = bulk_df.loc[bidx, ["x", "y", "z"]]
+			found = False
+			n_neighbor = 1
+			while not found:
+				dmins = get_d2surf(x=x, y=y, z=z, list_zfiles=list_zfiles, n_neighbor=n_neighbor)
+				dmin_val = np.nanmin(dmins)
+				print (x, y, z, "n_neighbor:",n_neighbor, "dmins:", dmins, "dmin_val:", dmin_val)
+				if math.isnan(dmin_val):
+					n_neighbor += 1
+				else:
+					df.loc[bidx, "dist2surf"] = dmin_val
+					found = True
+		# east, west, north, south, up, down
+		
+		# pool = multiprocessing.Pool(processes=128)
+		# results = pool.map(partial(get_dmin, bulk_df=bulk_df, list_zfiles=list_zfiles), bulk_idxs)
+		# # results = 0
+		# df.loc[bulk_idxs, "dist2surf"] = results
+
+
+		# run for layer_0 first
+		save_at = "{0}/csv_dist2surf/layer_{1}.csv".format(result_dir, z)
+		makedirs(save_at)
+		df.to_csv(save_at)
+		break
+
+
+if __name__ == "__main__":
+	sub_cats = dict({
+		"Co_density": ["e_Co_fresh", "e_Co_21k", "e_Co_34k"],
+		"Pt_density": ["e_Pt_fresh", "e_Pt_21k", "e_Pt_34k"],
+		"morphology": ["fresh", "21k", "34k"],
+		"Pt_valence": ["v_04_fresh", "v_04_21k", "v_04_34k", "v_10_fresh", "v_10_21k", "v_10_34k"],
+		"delta density": ["d_Co", "d_Pt"]
+
+		})
+	get_surf_lbl(main_cat="morphology", sub_cat="21k") # Pt_density, e_Pt_fresh
+	# Co_density, e_Co_fresh
+
+	get_dmins()
+
+
+	# get_xyz(index="x291_y10_z3")
---
+++ plot.py
@@ -0,0 +1,479 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time, gc, os
+import pandas as pd
+
+import sys
+sys.path.append("..")
+try:
+	import seaborn as sns
+except:
+	pass
+import itertools
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import stats
+
+axis_font = {'fontname': 'serif', 'size': 16, 'labelpad': 8}
+title_font = {'fontname': 'serif', 'size': 12}
+size_point = 3
+alpha_point = 0.3
+n_neighbor = 3
+
+
+def release_mem(fig):
+	fig.clf()
+	plt.close()
+	gc.collect()
+
+
+
+def ax_setting():
+	plt.style.use('default')
+	plt.tick_params(axis='x', which='major', labelsize=13)
+	plt.tick_params(axis='y', which='major', labelsize=13)
+
+
+def makedirs(file):
+	if not os.path.isdir(os.path.dirname(file)):
+		os.makedirs(os.path.dirname(file))
+
+
+def ax_setting_3d(ax):
+	ax.grid(which='minor', alpha=0.2)
+	ax.grid(which='major', alpha=0.5)
+	ax.xaxis._axinfo["grid"]['color'] =  "w"
+	ax.yaxis._axinfo["grid"]['color'] =  "w"
+	ax.zaxis._axinfo["grid"]['color'] =  "w"
+
+	# ax.set_xticks([])
+	# ax.set_zticks([])
+
+	ax.tick_params(axis='x', which='major', labelsize=20)
+	ax.tick_params(axis='y', which='major', labelsize=20)
+	ax.tick_params(axis='z', which='major', labelsize=20, pad=30)
+
+	ax.xaxis.pane.fill = False
+	ax.yaxis.pane.fill = False
+	ax.zaxis.pane.fill = False
+
+
+	ax.xaxis.pane.set_edgecolor('w')
+	ax.yaxis.pane.set_edgecolor('w')
+	ax.zaxis.pane.set_edgecolor('w')
+
+	ax.zaxis.set_rotate_label(False)
+
+	ax.view_init(elev=30.)
+	# ax.view_init(45, 120) # good for Tc
+	plt.tight_layout(pad=1.1)
+
+
+def plot_density(values, save_at,  cmap_name="Oranges", vmin=None, vmax=None):
+	# input: matrix [n_rows, n_cols] of any value
+	# output: figure
+	fig = plt.figure(figsize=(10, 10))
+
+	if vmin is None and vmax is None:
+		max_abs = 0.95 * max((abs(np.min(values)), abs(np.max(values))))
+		vmin = -max_abs
+		vmax = max_abs
+
+
+
+
+	# m = np.ma.masked_where(np.isnan(values),values)
+	# plt.pcolor(m)
+	# img = plt.imread(values, format='raw')
+
+	cmap = plt.get_cmap(cmap_name)
+	cmap.set_bad('white')
+	# img = cmap(values)
+	# print (img)
+	plt.imshow(values, cmap=cmap, interpolation='none', vmin=vmin, vmax=vmax)
+
+	# plt.colorbar()
+	plt.xlabel('x', **axis_font)
+	plt.ylabel('y', **axis_font)
+
+	title = save_at[save_at.find('result/') + len('result/'): ]
+	plt.title(title, **title_font)
+
+
+	makedirs(save_at)
+
+	save_file = "{0}_dens.pdf".format(save_at)
+	plt.savefig(save_file, transparent=False)
+	print ("Save file at:", save_file)
+	release_mem(fig)
+
+
+def plot_hist(x, save_at=None, label=None, nbins=50):
+
+	if save_at is not None:
+		fig = plt.figure(figsize=(16, 16))
+
+	# hist, bins = np.histogram(x, bins=300, normed=True)
+	# xs = (bins[:-1] + bins[1:])/2
+
+	# plt.bar(xs, hist,  alpha=1.0)
+	# y_plot = hist
+	y_plot, x_plot, patches = plt.hist(x, bins=nbins, histtype='step', # step, stepfilled, 'bar', 'barstacked'
+										density=True, label=label, log=False,  
+										color='black', #edgecolor='none',
+										alpha=1.0, linewidth=2)
+
+	#plt.xticks(np.arange(x_min, x_max, (x_max - x_min) / 30), rotation='vertical', size=6)
+	# plt.ylim([1, np.max(y_plot)*1.01])
+
+	plt.ylabel('Probability density', **axis_font)
+	plt.xlabel("Value", **axis_font)
+
+	ax_setting()
+
+	if save_at is not None:
+
+		save_file = "{0}_hist.pdf".format(save_at)
+
+		if not os.path.isdir(os.path.dirname(save_file)):
+			os.makedirs(os.path.dirname(save_file))
+		plt.savefig(save_file)
+		print ("Save file at:", "{0}".format(save_file))
+		release_mem(fig)
+
+	return y_plot
+
+
+
+def plt_hist_gmm(X_plot, save_fig_file, label, is_kde=False, 
+				is_gmm=True, n_components_gmm=3, save_gmm_file=None,
+				means_init=None, weighs_init=None):
+	from opt_GMM import opt_GMM
+
+	# save all predicted val to csv 
+
+	# X_plot = X_plot[X_plot < 600]
+	nbins = 200
+	x_lb = np.min(X_plot)
+	x_ub = np.max(X_plot)
+	bandwidth = (x_ub - x_lb) / nbins
+
+	fig = plt.figure(figsize=(8, 8))
+
+	# plot hist of ensembling
+	if True:
+		plot_hist(x=X_plot, label=label, nbins=nbins)
+
+	# plot kde
+	X_kde = np.linspace(x_lb, x_ub, nbins)[:, np.newaxis]
+
+	# plot KernelDensity
+	if is_kde: # set True
+		kde = KernelDensity(kernel="gaussian", 
+			bandwidth=bandwidth).fit(X_plot.reshape(-1, 1))
+		print (np.min(X_plot), np.max(X_plot))
+		log_dens = kde.score_samples(X_kde)
+		plt.plot(X_kde, np.exp(log_dens), '-', label="KDE", linewidth=3, c="black")
+
+
+	# plot GMM
+	if is_gmm:
+		ax_setting()
+		this_mean_init = None
+		this_weigh_init = None
+		try:
+			this_mean_init = means_init[test_point]
+			this_weigh_init = weighs_init[test_point]
+		except:
+			pass
+
+		# optimize
+		best_gmm = opt_GMM(X=X_plot.reshape(-1, 1), n_sampling=200, 
+			n_components=n_components_gmm, means_init=this_mean_init)
+		best_gmm.fit(X_plot.reshape(-1, 1))
+
+		# save
+		if save_gmm_file is not None:
+			pickle.dump(best_gmm, open(save_gmm_file, 'wb'))
+
+
+		weights = best_gmm.weights_
+		means = best_gmm.means_
+		covars = best_gmm.covariances_
+
+		print ("Best_GMM: ", weights, means, covars,)
+		
+		# plot gaussian components
+		colors = ["red", "blue", "green", "black", "orange", 
+				  "brown", "purple", "cyan", "teal", "wheat", 'mediumslateblue', 
+				  'mediumspringgreen', 'mediumturquoise', 'mediumvioletred',  
+				  'mintcream', 'mistyrose', 'moccasin', 'slateblue', 'slategray', 
+				  'slategrey', 'snow', 'springgreen', 'steelblue', 
+				  'tan', 'teal', 'thistle', 'tomato',]
+		for ii, (w, m, c, cl) in enumerate(zip(weights, means, covars, colors)):
+			# print (test_point, w, m, c)
+			this_X_plt = X_kde
+			plt.fill(this_X_plt, w*stats.norm.pdf(this_X_plt, m, np.sqrt(c)), 
+				label="Mean {0}: {1}".format(int(ii +1), round(m[0],2)), c=cl, alpha=0.8)
+		
+
+	
+	plt.legend(prop={'size': 16})
+	plt.savefig(save_fig_file)
+	release_mem(fig)
+
+
+
+
+
+
+def joint_plot(x, y, xlabel, ylabel, save_at):
+	fig = plt.figure(figsize=(20, 20))
+	# sns.set_style('ticks')
+	sns.plotting_context(font_scale=1.5)
+	this_df = pd.DataFrame()
+	
+	this_df[xlabel] = x
+	this_df[ylabel] = y
+
+	ax = sns.jointplot(this_df[xlabel], this_df[ylabel],
+					kind="kde", shade=True,
+					).set_axis_labels(xlabel, ylabel)
+
+	# ax.spines['right'].set_visible(False)
+	# ax.spines['top'].set_visible(False)
+	# plt.xlabel(r'%s' %xlabel, **axis_font)
+	# plt.ylabel(r'%s' %ylabel, **axis_font)
+	# plt.title(title, **self.axis_font)
+
+	# plt.set_tlabel('sigma', **axis_font)
+	# ax_setting()
+	save_file = "{0}_joint.pdf".format(save_at)
+	plt.tight_layout()
+	if not os.path.isdir(os.path.dirname(save_file)):
+		os.makedirs(os.path.dirname(save_file))
+	plt.savefig(save_file)
+
+	print ("Save file at:", "{0}".format(save_file))
+	release_mem(fig)
+# return ax
+
+
+
+def ax_histfill(x, label, ax, lbx, ubx, orientation="horizontal"):
+
+
+	y_plot, x_plot, patches = ax.hist(x, bins=100, histtype='bar', # step, stepfilled, 'bar', 'barstacked'
+				density=True, label=label, log=False,  
+				color='grey', #edgecolor='none',
+				alpha=0.5, linewidth=2, orientation=orientation)
+
+	# abs0_reg = np.where(np.abs(x_plot) < abs_zero)[0]
+
+	if lbx is not None and ubx is not None:	
+		selected_reg = np.where((x_plot > lbx) & (x_plot < ubx))[0]
+
+		for i in selected_reg:
+			patches[i].set_color('orange')
+
+	return ax
+
+
+def joint_plot_fill(x, y, xlabel, ylabel, save_at, lbx, ubx, lby, uby):
+	fig = plt.figure(figsize=(8, 8))
+
+	grid = plt.GridSpec(4, 4, hspace=0.3, wspace=0.3)
+	main_ax = fig.add_subplot(grid[1:, :-1])
+	y_hist = fig.add_subplot(grid[1:, -1], xticklabels=[], sharey=main_ax)
+	x_hist = fig.add_subplot(grid[0, :-1], yticklabels=[], sharex=main_ax)
+	
+
+	sns.set_style('ticks')
+
+	df = pd.DataFrame()
+	df[xlabel] = x
+	df[ylabel] = y
+	# print (this_df)
+	g = sns.jointplot(df[xlabel], df[ylabel],
+                     kind="kde", #ax=main_ax,
+                     cmap="Greys", stat_func=None, n_levels=20, # GnBu
+                     shade=True)
+
+	x_hist = g.ax_marg_x
+	y_hist = g.ax_marg_y
+
+	# main_ax = sns.kdeplot(x, y,
+		# this_df.loc[:, xlabel].values, this_df.loc[:, ylabel].values,
+				# kind="kde", 
+		# ax=main_ax, cmap="Greys")
+
+	x_hist = ax_histfill(x=x, label=xlabel, ax=x_hist, lbx=lbx, ubx=ubx, orientation='vertical')
+	y_hist = ax_histfill(x=y, label=ylabel, ax=y_hist, lbx=lby, ubx=uby, orientation='horizontal')
+	main_ax.tick_params(axis='both', labelsize=10)
+	main_ax.set_xlabel(xlabel, **axis_font)
+	main_ax.set_ylabel(ylabel, **axis_font)
+
+
+
+	plt.setp(x_hist.get_xticklabels(), visible=False)
+	plt.setp(y_hist.get_yticklabels(), visible=False)
+	
+
+	# sns.jointplot(x, y,kind="kde", shade=True)
+
+	plt.tight_layout(pad=1.1)
+
+	makedirs(save_at)
+	plt.savefig(save_at)
+	release_mem(fig=fig)
+
+
+class corr_analysis():
+	def __init__(self, data_file, threshold_pearson, threshold_spearman, all_variable, out_dir):
+		self.df = pd.read_csv(filepath_or_buffer=data_file, index_col=0)
+		#self.variables = list(self.df.columns)
+		if all_variable:
+			self.variables = self.df.columns
+		else:
+			self.variables = all_variable
+
+
+		self.instance_name = self.df.index
+		self.threshold_pearson = threshold_pearson
+		self.threshold_spearman = threshold_spearman
+		self.out_dir = out_dir
+
+		self.axis_font = {'fontname': 'serif', 'size': 14, 'labelpad': 10}
+
+		self.title_font = {'fontname': 'serif', 'size': 14}
+
+	def plot_correlation(self, x, y, xlabel, ylabel, title):
+		fig = plt.figure(figsize=(16, 16))
+		ax_setting()
+		# plt.scatter(x=x, y=y, s=30, c='blue', alpha=0.1, edgecolors='none')
+		sns.jointplot(x, y,  kind="kde", color="g")
+		plt.xlabel(r'%s' %xlabel, **self.axis_font)
+		plt.ylabel(r'%s' %ylabel, **self.axis_font)
+		plt.title(title, **self.axis_font)
+		plt.savefig("%s/%s vs %s.pdf" % (self.out_dir, xlabel, ylabel))
+		release_mem(fig)
+
+	def correlation_matrix(self):
+
+		all_var_pair = itertools.combinations(self.variables, r=2)
+
+		this_df_pearson = pd.DataFrame(index=self.variables, columns=self.variables)
+		this_df_spear = pd.DataFrame(index=self.variables, columns=self.variables)
+
+		for pair in all_var_pair:
+
+			x = self.df[pair[0]]
+			y = self.df[pair[1]]
+			pearson, p_value = stats.pearsonr(x=x, y=y)
+			spear1, p_value = stats.spearmanr(a=x, b=y)
+			spear2, p_value = stats.spearmanr(a=y, b=x)
+
+			# if pearson > threshold_pearson:
+			self.plot_correlation(x=x, y=y, 
+				xlabel=pair[0], ylabel=pair[1], 
+				title="Pearson {0}".format(round(pearson, 3)))
+
+			pearson = abs(pearson)
+			spear1 = abs(spear1)
+			spear2 = abs(spear2)
+
+			if pearson > self.threshold_pearson:
+				this_df_pearson[pair[0]][pair[1]] = pearson
+				this_df_pearson[pair[1]][pair[0]] = pearson
+			else:
+				this_df_pearson[pair[0]][pair[1]] = 0
+				this_df_pearson[pair[1]][pair[0]] = 0
+
+			if spear1 > self.threshold_spearman:
+				this_df_spear[pair[0]][pair[1]] = spear1
+			else:
+				this_df_spear[pair[0]][pair[1]] = 0
+
+			if spear2 > self.threshold_spearman:
+				this_df_spear[pair[1]][pair[0]] = spear2
+			else:
+				this_df_spear[pair[1]][pair[0]] = 0
+
+			this_df_pearson[pair[0]][pair[0]] = 1
+			this_df_pearson[pair[1]][pair[1]] = 1
+			this_df_spear[pair[0]][pair[0]] = 1
+			this_df_spear[pair[1]][pair[1]] = 1
+
+
+		this_df_pearson.to_csv("%s/Pearson.csv" %self.out_dir)
+		this_df_spear.to_csv("%s/Spearman.csv" %self.out_dir)
+
+	def plot_task3(self):
+
+		self.correlation_matrix()
+
+		for type_ in ["Pearson", "Spearman"]:
+			X = pd.read_csv("%s/%s.csv" %(self.out_dir, type_), index_col=0)
+			X_MDS = X.copy()
+
+			X = X[X > 0.7]
+
+			fig = plt.figure(figsize=(16, 16))
+			ax = sns.heatmap(X)
+
+
+			for item in ax.get_yticklabels():
+				item.set_fontsize(8)
+				item.set_rotation(0)
+				item.set_fontname('serif')
+
+			for item in ax.get_xticklabels():
+				item.set_fontsize(8)
+				item.set_fontname('serif')
+				item.set_rotation(90)
+
+			plt.title("%s similarity" %(type_), **self.title_font)
+			plt.savefig("%s/%s.eps" %(self.out_dir, type_))
+			release_mem(fig)
+
+
+
+
+
+
+
+def get_subaxis():
+	fig = plt.figure(figsize=(8, 8))
+	grid = plt.GridSpec(4, 4, hspace=0.3, wspace=0.3)
+	main_ax = fig.add_subplot(grid[1:, :-1])
+	x_axis = fig.add_subplot(grid[0, :-1], yticklabels=[], sharex=main_ax)
+	y_axis = fig.add_subplot(grid[1:, -1], xticklabels=[], sharey=main_ax)
+	sns.set_style('ticks')
+
+
+	return main_ax, x_axis. y_axis
+
+
+
+
+
+def merge_2image(morph_df, df2, vmin, vmax, title, save_at, cmapdf2='jet'):
+	# from matplotlib.colors import LightSource
+	fig = plt.figure(figsize=(10, 10))
+
+
+	plt.imshow(morph_df.values, cmap='binary')
+	plt.colorbar()
+
+	plt.imshow(df2.values, cmap=cmapdf2, interpolation='none', 
+		vmin=vmin, vmax=vmax)
+
+	# cmap.set_bad('white')
+	
+	# plt.colorbar()
+	plt.xlabel('x', **axis_font)
+	plt.ylabel('y', **axis_font)
+
+	plt.title(title, **title_font)
+
+	makedirs(save_at)
+	plt.savefig(save_at)
